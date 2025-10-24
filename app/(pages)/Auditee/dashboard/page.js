dd41c213fe2c193939f974bb649420e0a7454f0f
sanketPatel8 <sanket.xceptive@gmail.com>
2025-10-24 14:03:14 +0530

    update

--- app/(pages)/Auditee/dashboard/page.js
+++ app/(pages)/Auditee/dashboard/page.js
@@ -1,12 +1,33 @@
+import ActivityList from "@/components/common/ActivityList";
+import AuditList from "@/components/module/audit_schedule/AuditList";
+import Chart from "@/components/module/audit_schedule/Chart";
 import OverViewDetails from "@/components/module/audit_schedule/OverViewDetails";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <div className="row">
-        <div className="col-md-6">
-          <OverViewDetails />
+      <div className="w-full max-w-full">
+        <div className="row gap-0 my-2">
+          <div className="col-md-12">
+            <OverViewDetails />
+          </div>
+        </div>
+        <div className="row my-2">
+          <div className="col-md-6">
+            <Chart />
+          </div>
+          <div className="col-md-6">
+            <Chart />
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-md-6">
+            <ActivityList />
+          </div>
+          <div className="col-md-6">
+            <ActivityList />
+          </div>
         </div>
       </div>
     </>
